b7b5b2d4c32ec296a7de4bdefcc9630c2e7e2962
Abdulrahman aboudaoud <42177282+Daoud98@users.noreply.github.com>
2018-08-17 17:48:44 +0200

    Add files via upload
    This is pset 0 and pset 1 solution .

---
+++ cs50/mario.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("height: ");
+    }
+    while (n < 0 || n > 23);
+
+    for (int i = 0 ; i < n; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 0 ; j < i + 2; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+
+    }
+}
+
+
